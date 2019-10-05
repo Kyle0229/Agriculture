ce84a99b1bdb74322a5930a0d4a10414d86db22c
kyle <824238868@qq.com>
2019-10-05 13:38:08 +0800

    恢复原状

--- src/main/java/com/qf/web/GoodsController.java
+++ src/main/java/com/qf/web/GoodsController.java
@@ -1,22 +1,11 @@
 package com.qf.web;
 
-import com.qf.domain.Goods;
-import com.qf.service.GoodsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
+
 
 @RestController
 public class GoodsController {
-    @Resource
-    GoodsService goodsServiceImpl;
-    @RequestMapping("/selectOne")
-    public Goods selectOne(@RequestParam("id")Integer id){
-        Goods goods =goodsServiceImpl.selectOne(id);
-        return goods;
 
-    }
+
 }
