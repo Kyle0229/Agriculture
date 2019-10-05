f7ef59d243a4f4a755b213ab4975dc702e23d134
kyle <824238868@qq.com>
2019-10-05 13:28:40 +0800

    李鲲做了商品查询

--- src/main/java/com/qf/domain/Goods.java
+++ src/main/java/com/qf/domain/Goods.java
@@ -2,52 +2,61 @@ package com.qf.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Goods {
-    Integer gid;
-    String gname;
-    Float oaprice;
+    @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
+            @Column(name = "gid")
+    Integer id;
+    @Column(name = "gname")
+    String name;
+    @Column(name = "oaprice")
+    Float price;
+    @Column(name = "info")
     String info;
 
-    public Integer getGid() {
-        return gid;
-    }
-
-    public void setGid(Integer gid) {
-        this.gid = gid;
-    }
-
-    public String getGname() {
-        return gname;
-    }
-
-    public void setGname(String gname) {
-        this.gname = gname;
-    }
-
-    public Float getOaprice() {
-        return oaprice;
-    }
-
-    public void setOaprice(Float oaprice) {
-        this.oaprice = oaprice;
-    }
-
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
-    }
-
-    @Override
-    public String toString() {
-        return "Goods{" +
-                "gid=" + gid +
-                ", gname='" + gname + '\'' +
-                ", oaprice=" + oaprice +
-                ", info='" + info + '\'' +
-                '}';
-    }
+//  info  public Integer getGid() {
+//        return gid;
+//    }
+//
+//    public void setGid(Integer gid) {
+//        this.gid = gid;
+//    }
+//
+//    public String getGname() {
+//        return gname;
+//    }
+//
+//    public void setGname(String gname) {
+//        this.gname = gname;
+//    }
+//
+//    public Float getOaprice() {
+//        return oaprice;
+//    }
+//
+//    public void setOaprice(Float oaprice) {
+//        this.oaprice = oaprice;
+//    }
+//
+//    public String getInfo() {
+//        return info;
+//    }
+//
+//    public void setInfo(String info) {
+//        this.info = info;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Goods{" +
+//                "gid=" + gid +
+//                ", gname='" + gname + '\'' +
+//                ", oaprice=" + oaprice +
+//                ", info='" + info + '\'' +
+//                '}';
+//    }
 }
