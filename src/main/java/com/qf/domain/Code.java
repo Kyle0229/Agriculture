68544fd2e8c3eae18fbbce80ad01ada2137df506
denglei <744882124@qq.com>
2019-10-09 17:31:04 +0800

    ss

--- src/main/java/com/qf/domain/Code.java
+++ src/main/java/com/qf/domain/Code.java
@@ -17,4 +17,55 @@ public class Code {
    // @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date createTime;
     private Integer status;  //状态码，1 有效，0 失效
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Code{" +
+                "id=" + id +
+                ", userEmail='" + userEmail + '\'' +
+                ", code='" + code + '\'' +
+                ", createTime=" + createTime +
+                ", status=" + status +
+                '}';
+    }
 }
